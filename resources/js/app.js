c38530deeeab1a724569a7b862c15ed701f0fc27
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2019-07-10 14:23:08 +0700

    move components to sub-folder

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,11 +14,11 @@ Vue.component('carousel', require('./components/carousel.vue').default);
 
 
 Vue.component('product', require('./components/page/product.vue').default);
-Vue.component('user-manage', require('./components/page/userManage.vue').default);
-Vue.component('transaction', require('./components/page/transaction.vue').default);
-Vue.component('report-user', require('./components/page/reportforuser.vue').default);
-Vue.component('sale-report', require('./components/page/salereport.vue').default);
-Vue.component('dividend-for-admin', require('./components/page/dividendforadmin.vue').default);
+Vue.component('user-manage', require('./components/page/staff/userManage.vue').default);
+Vue.component('transaction', require('./components/page/staff/transaction.vue').default);
+Vue.component('report-user', require('./components/page/user/reportforuser.vue').default);
+Vue.component('sale-report', require('./components/page/staff/salereport.vue').default);
+Vue.component('dividend-for-admin', require('./components/page/staff/dividendforadmin.vue').default);
 
 
 
